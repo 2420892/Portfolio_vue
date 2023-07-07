5fedb09130fd0d36079abaae667088123de5ec6d
siyamanga22 <zwenisiyamanga@gmail.com>
2023-07-07 09:13:16 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-    <nav-bar/>
-  <router-view/>
-  <footer-comp/>
+  <nav-bar />
+  <router-view />
+  <footer-comp />
 </template>
 <script>
-import NavBar from './components/NavBar.vue';
-import FooterComp from './components/FooterComp.vue';
-export default{
-  components:{
+import NavBar from "./components/NavBar.vue";
+import FooterComp from "./components/FooterComp.vue";
+export default {
+  components: {
     NavBar,
     FooterComp,
-  }
-}
+  },
+};
 </script>
-
 
 <style>
 #app {
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-md-4 mb-4" v-for="project in projects" :key="project.id">
         <div class="card" style="width:18rem">
-          <img :src="project.image" class="card-img-top img-fluid"  :alt="project.name" style="height:200px; width:18rem">
+          <img :src="project.image" class="card-img-top img-fluid" :alt="project.name" style="height:200px; width:18rem">
           <div class="card-body">
             <h5 class="card-title">{{ project.name }}</h5>
             <a :href="project.github" target="blank" class="card-link">View Code</a>
@@ -18,36 +18,38 @@
 
 
 
-  <script>
-  export default{
-    computed:{
-      projects(){
-        return this.$store.state.projects
-      }
-    },
-    mounted(){
-      this.$store.dispatch('fetchProjects')
+<script>
+export default {
+  computed: {
+    projects() {
+      return this.$store.state.projects
     }
-   
+  },
+  mounted() {
+    this.$store.dispatch('fetchProjects')
   }
-  </script>
-  <style scoped>
-  
-  .card{
-    background-color:rgba(222,227,229,255);
-    transition: transform .2s; 
-  }
-  .card:hover {
-    transform: scale(1.1); 
-  }
-@media (width<800px){
-.card{
-width:auto!important;
+
 }
-.card img{
-  width:auto!important;
-  object-fit: cover;
+</script>
+<style scoped>
+.card {
+  background-color: rgba(222, 227, 229, 255);
+  transition: transform .2s;
 }
+
+.card:hover {
+  transform: scale(1.1);
+}
+
+@media (width<800px) {
+  .card {
+    width: auto !important;
   }
-  </style>
+
+  .card img {
+    width: auto !important;
+    object-fit: cover;
+  }
+}
+</style>
   
